afd5b3e2acfc92535fc968d847812a0a70a2d18f
unknown <NLandis@landis-dev-vm.twelveinteractive.office>
2014-08-24 15:10:13 -0500

    Event continuation settings for IHandlers Execution order to assert that listeners get to see all events

--- CopyMasta/CopyMasta/Core/Handler/BucketHandler.cs
+++ CopyMasta/CopyMasta/Core/Handler/BucketHandler.cs
@@ -9,11 +9,13 @@ namespace CopyMasta.Core.Handler
 {
     public class BucketHandler : IHandler
     {
+        public int AbsoluteExecutionOrder { get { return ExecutionOrders.BucketHandler; } }
+
         private readonly IDictionary<char, string> _buckets = new Dictionary<char, string>();
         private readonly ICollection<char> _targets = new[] { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
         private readonly object _locker = new object();
 
-        public void Handle(KeyState state)
+        public EventContinuation Handle(KeyState state)
         {
             if (!Clipboard.ContainsText() 
                 || !state.MetaKeys.HasFlag(MetaKeys.Ctrl) 
@@ -21,7 +23,7 @@ namespace CopyMasta.Core.Handler
                 || state.Keys.Count != 1
                 || !_targets.Contains(state.Keys[0]))
             {
-                return;
+                return EventContinuation.Continue;
             }
 
             lock (_locker)
@@ -29,12 +31,16 @@ namespace CopyMasta.Core.Handler
                 if (!state.MetaKeys.HasFlag(MetaKeys.Shift))
                 {
                     _buckets[state.Keys[0]] = Clipboard.GetText();
+                    return EventContinuation.Abort;
                 }
                 else if (_buckets.ContainsKey(state.Keys[0]))
                 {
                     Clipboard.SetText(_buckets[state.Keys[0]]);
+                    return EventContinuation.Abort;
                 }
             }
+
+            return EventContinuation.Continue;
         }
     }
 }
--- CopyMasta/CopyMasta/Core/Handler/ConsoleDebuggerHandler.cs
+++ CopyMasta/CopyMasta/Core/Handler/ConsoleDebuggerHandler.cs
@@ -8,13 +8,17 @@ namespace CopyMasta.Core.Handler
 {
     public class ConsoleDebuggerHandler : IHandler
     {
-        public void Handle(KeyState state)
+        public int AbsoluteExecutionOrder { get { return ExecutionOrders.ConsoleDebugger; } }
+
+        public EventContinuation Handle(KeyState state)
         {
             Console.WriteLine("Alt: {0}\tCtrl: {1}\tShift: {2}\tLetters: {3}",
                               state.MetaKeys.HasFlag(MetaKeys.Alt) ? "1" : "0",
                               state.MetaKeys.HasFlag(MetaKeys.Ctrl) ? "1" : "0",
                               state.MetaKeys.HasFlag(MetaKeys.Shift) ? "1" : "0",
                               string.Join(", ", state.Keys));
+
+            return EventContinuation.Continue;
         }
     }
 }

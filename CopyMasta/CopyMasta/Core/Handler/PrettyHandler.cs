9623c4b5f796adce75e25f7971d6f100328fb09c
Ryan Stelly <rstelly@spotlite.com>
2014-10-22 15:55:20 -0500

    Pretty handler: ctrl+alt+shift+p the unescape clip contents

---
+++ CopyMasta/CopyMasta/Core/Handler/PrettyHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CopyMasta.Core.Handler
+{
+    public class PrettyHandler : IHandler
+    {
+        public int AbsoluteExecutionOrder { get { return ExecutionOrders.PrettyHandler; } }
+
+        public EventContinuation Handle(KeyState state, bool isActiveTransition)
+        {
+            if (!isActiveTransition)
+            {
+                return EventContinuation.Continue;
+            }
+
+            if (!state.MetaKeys.HasFlag(MetaKeys.Alt)
+                || !state.MetaKeys.HasFlag(MetaKeys.Ctrl)
+                || !state.MetaKeys.HasFlag(MetaKeys.Shift)
+                || !state.Keys.Contains('P')
+                || !Clipboard.ContainsText())
+            {
+                return EventContinuation.Continue;
+            }
+
+            Clipboard.SetText(Regex.Unescape(Clipboard.GetText()));
+
+            return EventContinuation.Continue;
+        }
+    }
+}
